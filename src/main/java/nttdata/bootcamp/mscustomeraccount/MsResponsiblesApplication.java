21d43ba5af0ec2d864b5cb6b17b474e3934afe29
TheoRevFdz <hrevillafernandez79@gmail.com>
2022-10-26 20:15:04 -0500

    Correccion del nombre del proyecto

--- src/main/java/nttdata/bootcamp/mscustomeraccount/MsResponsiblesApplication.java
+++ src/main/java/nttdata/bootcamp/mscustomeraccount/MsResponsiblesApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class MsCustomerAccountApplication {
+public class MsResponsiblesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MsCustomerAccountApplication.class, args);
+		SpringApplication.run(MsResponsiblesApplication.class, args);
 	}
 
 }
